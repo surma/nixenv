fc5a914b81148e2c3019e0235504c5f054f2e97f
Surma <surma@surma.dev>
2025-09-06 14:10:42 +0100

    Make SSH key private

--- home-manager/ssh-keys.nix
+++ home-manager/ssh-keys.nix
@@ -9,6 +9,7 @@
       command = ''
         cp ${../ssh-keys/id_surma.pub} ${config.home.homeDirectory}/.ssh/id_surma.pub
         cat > ${config.home.homeDirectory}/.ssh/id_surma
+        chmod 0600 ${config.home.homeDirectory}/.ssh/id_surma
       '';
     };
   };
