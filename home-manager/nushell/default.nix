0b3115540ef0ea182e407c5f5e8c9319fe41792b
Surma <surma@surma.dev>
2025-11-11 13:06:59 +0000

    Add ngco

--- home-manager/nushell/default.nix
+++ home-manager/nushell/default.nix
@@ -14,6 +14,10 @@ with lib;
       def ngs [...args] {
         git status --porcelain ...$args | from ssv -n -m 1 | rename status path | update path { [(git rev-parse --show-toplevel) $in] | path join }
       }
+
+      def ngco {
+        git for-each-ref refs/heads | from tsv -n | rename meta ref |  update ref { $in | str substring 11.. } | get ref | sort | str join "\n" | fzf | if ($in|str length) > 0 {git checkout $in} else {print "Aborted."};
+      }
     '';
   };
 }
