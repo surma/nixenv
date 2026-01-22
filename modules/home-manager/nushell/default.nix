ed37703eb41374a4271475f37d4ae70eec5b4d93
Surma <surma@surma.dev>
2026-01-22 11:02:09 +0000

    Make ngb configurable

--- modules/home-manager/nushell/default.nix
+++ modules/home-manager/nushell/default.nix
@@ -5,8 +5,8 @@
         git status --porcelain ...$args | from ssv -n -m 1 | rename status path | update path { [(git rev-parse --show-toplevel) $in] | path join }
       }
 
-      def ngb [] {
-        git for-each-ref refs/heads | from tsv -n | rename meta ref |  update ref { $in | str substring 11.. } | get ref | sort | str join "\n" | fzf
+      def --wrapped ngb [...args] {
+        git for-each-ref refs/heads | from tsv -n | rename meta ref |  update ref { $in | str substring 11.. } | get ref | sort | str join "\n" | fzf ...$args
       }
 
       def ngco [] {
