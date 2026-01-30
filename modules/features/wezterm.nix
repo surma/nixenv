5449953609dcd05ff0d3097fbc63e8aa06856aad
Surma <surma@surma.dev>
2026-01-30 10:51:49 +0000

    Fix wezterm feature module - remove conditional options

--- modules/features/wezterm.nix
+++ modules/features/wezterm.nix
@@ -5,32 +5,30 @@ with lib;
     ../home-manager/wezterm/default-config.nix
   ];
 
-  options = mkIf (systemManager == "home-manager") {
-    programs.wezterm = {
-      frontend = mkOption {
-        type = with types; enum [ "WebGpu" "OpenGL" ];
-        default = "WebGpu";
-      };
-      fontSize = mkOption {
-        type = types.int;
-        default = 12;
-      };
-      window-decorations = mkOption {
-        type = with types; nullOr (listOf (enum [ "RESIZE" "TITLE" ]));
-        default = [ "TITLE" "RESIZE" ];
-      };
-      dark-theme = mkOption {
-        type = types.str;
-        default = "Gruvbox Dark (Gogh)";
-      };
-      light-theme = mkOption {
-        type = types.str;
-        default = "Gruvbox (Gogh)";
-      };
-      theme = mkOption {
-        type = with types; enum [ "dark" "light" "auto" ];
-        default = "auto";
-      };
+  options.programs.wezterm = {
+    frontend = mkOption {
+      type = with types; enum [ "WebGpu" "OpenGL" ];
+      default = "WebGpu";
+    };
+    fontSize = mkOption {
+      type = types.int;
+      default = 12;
+    };
+    window-decorations = mkOption {
+      type = with types; nullOr (listOf (enum [ "RESIZE" "TITLE" ]));
+      default = [ "TITLE" "RESIZE" ];
+    };
+    dark-theme = mkOption {
+      type = types.str;
+      default = "Gruvbox Dark (Gogh)";
+    };
+    light-theme = mkOption {
+      type = types.str;
+      default = "Gruvbox (Gogh)";
+    };
+    theme = mkOption {
+      type = with types; enum [ "dark" "light" "auto" ];
+      default = "auto";
     };
   };
 }
