ea5155e57508e83e851519e6eaef48438e22567b
Surma <surma@surma.dev>
2026-03-06 19:19:25 +0000

    New scout hostkey

--- secrets/config.nix
+++ secrets/config.nix
@@ -11,7 +11,7 @@
     archon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGHVSc2lTehmhEl87rp3m15b8Q1GojwNJsnbxJxWe99";
     surmturntable = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBb7S7oe5a61I0TH+2xmI68rGVflyftTvjQlVinJgFOr surma@surmturntable";
     nexus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFSKtxq/1aLxos5lZTWzROCqMLkiVlxKd1jJF0XKGCrW surma@nexus";
-    scout = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGaIcMYnbJV/JbZL/d1f7xmIspKM41/F/OdN5BnepduM";
+    scout = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbMZPClNS+nP2Khjn6qngXXUsmW6rbRmJ3TewYdTxRS";
   };
   secrets = {
     ssh-keys = {
