fbd9d12825f08cd6111037bab8b5d305e35353a5
Surma <surma@surma.dev>
2026-04-12 18:33:30 +0100

    Fix gitea

--- machines/nexus/service-gitea.nix
+++ machines/nexus/service-gitea.nix
@@ -14,8 +14,12 @@ in
       services.gitea.stateDir = "/dump/state/gitea";
       services.gitea.user = "containeruser";
       services.gitea.settings.server.HTTP_PORT = 8080;
+      services.gitea.settings.server.DOMAIN = "gitea.surma.technology";
+      services.gitea.settings.server.ROOT_URL = "https://gitea.surma.technology/";
+      services.gitea.settings.server.SSH_DOMAIN = "gitea.surma.technology";
       services.gitea.settings.server.SSH_PORT = ports.giteaSsh;
       services.gitea.settings.server.START_SSH_SERVER = true;
+      services.gitea.settings.database.SQLITE_JOURNAL_MODE = "WAL";
       # services.openssh.enable = true;
     };
 
