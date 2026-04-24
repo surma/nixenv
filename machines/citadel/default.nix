87d1b10897b93f065ffe82d22b565822fe46b45a
Surma <surma@surma.dev>
2026-04-24 16:24:19 +0100

    Reenable graphical

--- machines/citadel/default.nix
+++ machines/citadel/default.nix
@@ -6,6 +6,14 @@
   imports = [
     ./hardware.nix
     ../../profiles/nixos/base.nix
+
+    ../../modules/nixos/hyprland
+    ../../modules/nixos/1password-wrapper
+  ];
+
+  allowedUnfreeApps = [
+    "1password"
+    "1password-cli"
   ];
 
   networking.hostName = "citadel";
@@ -20,11 +28,57 @@
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  boot.kernel.sysctl = {
+    "kernel.dmesg_restrict" = 0;
+  };
+
   networking.firewall.enable = false;
   networking.networkmanager.enable = true;
   programs.nm-applet.enable = true;
 
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  services.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   services.openssh.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    hyprpolkitagent
+    hyprlock
+    pavucontrol
+    hyprsunset
+    pciutils
+    usbutils
+  ];
+
+  programs._1password.enable = true;
+  programs._1password-gui.enable = true;
+  programs._1password-gui.polkitPolicyOwners = [ "surma" ];
+
+  programs.obs.enable = true;
+  programs.firefox.enable = true;
+  programs.signal.enable = true;
+
+  security.polkit.enable = true;
+  security.pam.services.hyprlock = { };
+
+  users.users.surma.extraGroups = [
+    "networkmanager"
+    "wheel"
+    "input"
+    "video"
+    "audio"
+  ];
+
+  services.udisks2.enable = true;
 
   system.stateVersion = "25.05";
 
--- machines/citadel/home.nix
+++ machines/citadel/home.nix
@@ -1,22 +1,87 @@
 {
   config,
+  pkgs,
   ...
 }:
 {
   imports = [
+    ../../scripts
+
     ../../profiles/home-manager/base.nix
     ../../profiles/home-manager/ai.nix
     ../../profiles/home-manager/linux.nix
     ../../profiles/home-manager/dev.nix
     ../../profiles/home-manager/nixdev.nix
+    ../../profiles/home-manager/graphical.nix
     ../../profiles/home-manager/workstation.nix
+    ../../profiles/home-manager/experiments.nix
+
+    ../../profiles/home-manager/webapps.nix
   ];
 
-  home.stateVersion = "25.05";
+  config = {
+    allowedUnfreeApps = [
+      "obsidian"
+    ];
 
-  home.sessionVariables.FLAKE_CONFIG_URI = "path:${config.home.homeDirectory}/src/github.com/surma/nixenv#citadel";
+    customScripts.toggle-sunset.enable = true;
+    customScripts.toggle-sunset.asDesktopItem = true;
+    customScripts.wallpaper-shuffle.enable = true;
+    customScripts.wallpaper-shuffle.asDesktopItem = true;
 
-  secrets.items.llm-proxy-client-key.target = "${config.home.homeDirectory}/.local/state/llm-proxy-client-key";
-  defaultConfigs.pi.extensions.proxy.enable = true;
+    home.packages = (
+      with pkgs;
+      [
+        chromium
+        kdePackages.dolphin
+        vlc
+        qview
+      ]
+    );
 
+    gtk = {
+      enable = true;
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
+    };
+
+    home.stateVersion = "25.05";
+
+    home.sessionVariables.FLAKE_CONFIG_URI = "path:${config.home.homeDirectory}/src/github.com/surma/nixenv#citadel";
+
+    programs.telegram.enable = true;
+    programs.whatsapp.enable = true;
+    programs.squoosh.enable = true;
+    programs.geforce-now.enable = true;
+    programs.xbox-remote-play.enable = true;
+    programs.obsidian.enable = false;
+
+    programs.wezterm.enable = true;
+    programs.wezterm.frontend = "OpenGL";
+    programs.wezterm.theme = "dark";
+    programs.wezterm.fontSize = 10;
+    programs.wezterm.window-decorations = null;
+    defaultConfigs.wezterm.enable = true;
+
+    programs.zellij.wl-clipboard.enable = true;
+
+    secrets.items.llm-proxy-client-key.target = "${config.home.homeDirectory}/.local/state/llm-proxy-client-key";
+
+    programs.pi.enable = true;
+    defaultConfigs.pi.enable = true;
+    defaultConfigs.pi.extensions.proxy.enable = true;
+
+    wayland.windowManager.hyprland.enable = true;
+    defaultConfigs.hyprland.enable = true;
+    programs.waybar.enable = true;
+    defaultConfigs.waybar.enable = true;
+    programs.hyprsunset.enable = true;
+    programs.hyprpaper.enable = true;
+    defaultConfigs.hyprpaper.enable = true;
+
+    services.blueman-applet.enable = true;
+    services.dunst.enable = true;
+  };
 }
