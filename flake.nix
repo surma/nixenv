8a415fdccf1dd7ace6c071a57d6b7baf8835b357
Surma <surma@surma.dev>
2026-05-03 00:06:08 +0100

    nixos-admin-web: point to feat/timer-triggering branch
    Adds systemd timer listing (GET /api/timers) and manual trigger
    (POST /api/timers/{unit}/trigger) endpoints, plus a Timers tab in the web UI.

--- flake.nix
+++ flake.nix
@@ -85,7 +85,7 @@
       inputs.flake-utils.follows = "flake-utils";
     };
     nixos-admin-web = {
-      url = "git+ssh://containeruser@gitea.surma.technology:2222/surma/nixos-admin-web.git?ref=main";
+      url = "git+ssh://containeruser@gitea.surma.technology:2222/surma/nixos-admin-web.git?ref=feat/timer-triggering";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
