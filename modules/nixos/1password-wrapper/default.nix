332119b2a689d2bdbd94a5149162e1078f1321ac
Surma <surma@surma.dev>
2026-05-09 09:26:29 +0100

    Autostart 1password

--- modules/nixos/1password-wrapper/default.nix
+++ modules/nixos/1password-wrapper/default.nix
@@ -6,13 +6,14 @@
 }:
 let
   inherit (pkgs) makeDesktopItem;
+  onepasswordCommand = "${config.programs._1password-gui.package}/bin/1password --ozone-platform=x11";
 in
 {
   environment.systemPackages = [
     (makeDesktopItem {
       name = "1password-wrapper";
       desktopName = "1Password (patched)";
-      exec = "${config.programs._1password-gui.package}/bin/1password --ozone-platform=x11";
+      exec = onepasswordCommand;
     })
   ];
 
@@ -22,5 +23,11 @@ in
       programs.ssh.matchBlocks."*".extraOptions = {
         "IdentityAgent" = ''"${config.home.homeDirectory}/.1password/agent.sock"'';
       };
+
+      # Autostart 1Password (patched) on Hyprland login. --silent keeps it
+      # in the tray instead of popping a window on every login.
+      wayland.windowManager.hyprland.extraConfig = ''
+        exec-once = ${onepasswordCommand} --silent
+      '';
     };
 }
