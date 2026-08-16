b79a86f44d9b1239f011e1b2e30881ee9d8177ca
Surma <surma@surma.dev>
2026-08-16 19:35:38 +0100

    More tuning

--- modules/home-manager/hyprland/default-config.nix
+++ modules/home-manager/hyprland/default-config.nix
@@ -25,7 +25,7 @@
         general = {
           lock_cmd = "pidof hyprlock || hyprlock";
           before_sleep_cmd = "loginctl lock-session";
-          after_sleep_cmd = "hyprctl dispatch dpms on";
+          after_sleep_cmd = "hyprctl dispatch 'hl.dsp.dpms({ action = \"on\" })'";
         };
 
         listener = [
@@ -35,8 +35,8 @@
           }
           {
             timeout = 330;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
+            on-timeout = "hyprctl dispatch 'hl.dsp.dpms({ action = \"off\" })'";
+            on-resume = "hyprctl dispatch 'hl.dsp.dpms({ action = \"on\" })'";
           }
         ];
       };
