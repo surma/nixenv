de5b4c49676a8877d0bf8031528284fd156a7a3f
Surma <surma@surma.dev>
2026-09-13 20:13:22 +0100

    fix(nexus): disable disconnected UPS services

--- machines/nexus/default.nix
+++ machines/nexus/default.nix
@@ -26,7 +26,8 @@
     ./service-vsftpd.nix
     ./service-redis.nix
     ./service-postgresql.nix
-    ./service-ups.nix
+    # Disabled while the disconnected UPS awaits replacement.
+    # ./service-ups.nix
     ./service-dump.nix
     ./service-overview.nix
     ./service-github-runner.nix
