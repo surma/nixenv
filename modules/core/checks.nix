26496b591a530f52fde247585a141055fc8561f5
Surma <surma@surma.dev>
2026-09-14 11:04:48 +0100

    refactor: remove local surmhosting implementation

--- modules/core/checks.nix
+++ modules/core/checks.nix
@@ -1,18 +1,2 @@
-{ lib, ... }:
-{
-  perSystem =
-    {
-      pkgs,
-      config,
-      ...
-    }:
-    {
-      checks.surmhosting-module = (import ../../modules/services/surmhosting/tests { inherit pkgs; }).all;
-      checks.surm-auth-e2e = pkgs.callPackage ../../modules/services/surmhosting/nix/checks/surm-auth-e2e.nix {
-        surm-auth = config.packages.surm-auth;
-      };
-      checks.surmhosting-auth-container = lib.mkIf (pkgs.system == "x86_64-linux") (
-        pkgs.callPackage ../../modules/services/surmhosting/tests/auth-container.nix { }
-      );
-    };
-}
+{ ... }:
+{ }
