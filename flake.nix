5dd2f70fbdc816786ec48b77a70be8f2fb4fc751
Surma <surma@surma.dev>
2026-09-16 19:57:07 +0100

    Deploy Immich on Nexus

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-immich.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
--- machines/nexus/default.nix
+++ machines/nexus/default.nix
@@ -13,6 +13,7 @@
     ./service-scout.nix
     ./service-gitea.nix
     ./service-hedgedoc2.nix
+    ./service-immich.nix
     ./service-opengist.nix
     ./service-lidarr.nix
     ./service-radarr.nix
